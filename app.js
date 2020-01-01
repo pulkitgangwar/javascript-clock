e3aea4a0b3fe1f627cbb1918d60ff6a389ce0bde
brainfrrreezer <brainfrrreezzer@gmail.com>
2020-01-01 17:59:56 +0530

    Init commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const clockContainer = document.querySelector(".clock");
+const clockFace = document.querySelector(".clock__face");
+
+const setClock = () => {
+  const time = new Date();
+  let hours = (time.getHours() % 12).toString();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds().toString();
+
+  if (hours.length < 2) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes.length < 2) {
+    minutes = `0${minutes}`;
+  }
+
+  if (seconds.length < 2) {
+    seconds = `0${seconds}`;
+  }
+
+  clockFace.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+};
+
+setClock();
+
+setInterval(setClock, 1000);
